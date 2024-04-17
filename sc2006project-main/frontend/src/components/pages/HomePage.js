582905ff92f2f1e9f14e2d6a115a59aef12d9715
chee cheng <122721995+cheechengm@users.noreply.github.com>
2024-04-17 09:49:17 +0800

    Update HomePage.js

--- sc2006project-main/frontend/src/components/pages/HomePage.js
+++ sc2006project-main/frontend/src/components/pages/HomePage.js
@@ -113,7 +113,7 @@ function HomePage() {
     };
 
     try {
-      const response = await fetch(`http://localhost:4000/api/search`, {
+      const response = await fetch(`https://sc2006projectwebsitedeployfrontend.onrender.com/api/search`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
